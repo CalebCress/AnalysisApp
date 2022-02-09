4dec242c87dee1ebe2d55df0bc2b9cb4073cabdf
calebc <calebc1@protonmail.com>
2022-02-09 12:13:48 -0800

    implemented sorting teams by event

--- src/routes/Teams.svelte
+++ src/routes/Teams.svelte
@@ -3,18 +3,50 @@
     import { data } from '../stores'
 
     let selected
+    let teams = $data.teams
 
+    function updateTeams() {
+        if (selected != undefined){
+            teams = []
+            if (selected === 0) {
+                teams = $data.teams
+                console.log(teams)
+            } else {
+                let teamNumbers
+                $data.events.every(eventObj => {
+                    if (eventObj.id === selected) {
+                        teamNumbers = eventObj.teams
+                        return false
+                    }
+                    return true
+                })
+                teamNumbers.forEach(teamNum => {
+                    let team
+                    $data.teams.every((teamIn => {
+                        if (teamIn.number === teamNum) {
+                            team = teamIn
+                            return false
+                        }
+                        return true
+                    }))
+                    teams.push(team)
+                })
+            }
+        }
+    }
 </script>
 <main>
     <div class="select">
-        <select bind:value={selected}>
+        <select bind:value={selected} on:change={updateTeams}>
             <option value={0}>All</option>
             {#each $data.events as event}
                 <option value={event.id}>{event.name}</option>
             {/each}
         </select>
       </div>
-    {#each $data.teams as team}
-        <Team teamData={team}/>
-    {/each}
+    {#if teams}
+        {#each teams as team}
+            <Team teamData={team}/>
+        {/each}
+    {/if}
 </main>
