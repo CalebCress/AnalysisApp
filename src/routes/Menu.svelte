fc9683cf9b888b491f74f2d9767fa6c2a8e62702
calebc <calebc1@protonmail.com>
2022-02-09 06:39:00 -0800

    Adding import data from file options

--- src/routes/Menu.svelte
+++ src/routes/Menu.svelte
@@ -1,10 +1,24 @@
+<script>
+  import {data} from '../stores.js'
+  let files
+  $: {
+    if (files && files.length > 0) {
+      new Response(files[0]).json().then(json => {
+        data.set(json)
+      }, err => {
+        console.log("not json")
+      })
+    }
+  }
+</script>
+
 <main>
     <div class="box">
       <h1 class="title">Load Data</h1>
       <nav class="level">
         <div class="file">
           <label class="file-label">
-            <input class="file-input" type="file" name="resume">
+            <input class="file-input" type="file" bind:files >
             <span class="file-cta">
               <span class="file-icon">
                   <i class="gg-file"></i>
